f893f6829e69ed49871eb3e9ac55928ddf7bd80b
brunoleo223 <brunoleo223@gmail.com>
2022-06-27 22:20:22 -0300

    trabalhando com paginação da api

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
@@ -7,15 +8,30 @@ import Restaurante from './Restaurante';
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [nextPage, setNextPage] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/v1/pratos/')
-      .then(response => setRestaurantes(response.data.results));
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/pratos/')
+      .then(response => {
+        setRestaurantes(response.data.results);
+        setNextPage(response.data.next);
+      })
+      .catch(err => console.log(err));
   }, [])
+
+  function verMais(){
+    axios.get<IPaginacao<IRestaurante>>(nextPage)
+      .then(response => {
+        setRestaurantes([...restaurantes, ...response.data.results]);
+        setNextPage(response.data.next);
+      })
+      .catch(err => console.log(err));
+  }
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {nextPage && <button onClick={verMais}>Ver mais</button>}
   </section>)
 }
 
